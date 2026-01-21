691eaf60422c348f40603a1d36f991b4971e7656
cedarwu <cedarwu@tencent.com>
2026-01-21 20:11:27 +0800

    disable checkSignature for local debug

--- src/android/Wechat.java
+++ src/android/Wechat.java
@@ -172,7 +172,7 @@ public class Wechat extends CordovaPlugin {
             String appId = getSavedAppId(ctx);
 
             if (!appId.isEmpty()) {
-                wxAPI = WXAPIFactory.createWXAPI(ctx, appId, true);
+                wxAPI = WXAPIFactory.createWXAPI(ctx, appId, false);
 
                 // 获取微信客户端版本
                 int clientVersion = wxAPI.getWXAppSupportAPI();
@@ -862,7 +862,7 @@ public class Wechat extends CordovaPlugin {
         currentCallbackContext = callbackContext;
         String appId = getAppId(preferences);
         ; // 填应用AppId
-        IWXAPI api = WXAPIFactory.createWXAPI(cordova.getActivity(), appId);
+        IWXAPI api = WXAPIFactory.createWXAPI(cordova.getActivity(), appId, false);
 
         final JSONObject params;
         try {
